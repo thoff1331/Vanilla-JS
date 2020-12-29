e3b3142ac1274c677120bc0a95dc6633a43aa299
Trevor Hoffman <trevorrhoffman@gmail.com>
2020-12-29 23:07:58 +0000

    add circle of numbers

--- Toy Problems/circleOfNumbers.js
+++ Toy Problems/circleOfNumbers.js
@@ -4,9 +4,4 @@ circleOfNumbers = (n,firstNumber) => {
   for(let i = 0; i < n; i++) {
     numArray.push(i)
   }
-  if(firstNumber < firstNumber) {
-    return numArray[firstNumber + half]
-  } 
-  return numArray[firstNumber - half]
-  }
 }
